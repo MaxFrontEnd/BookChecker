80fb892476479841de4517fa6b4a517cd143414a
Max <maxidromlordus@mail.ru>
2018-10-03 16:21:04 +0300

    added switch books form one side to another

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ const readedCol = document.querySelector('.readed');
 const notReadedCol = document.querySelector('.not-readed');
 const move = document.querySelector('.move');
 const addBookButton = document.getElementById('add-book');
+
+
+//евенты
+function addEvents(){
+  // Добавить книгу в непрочитанное
+  addBookButton.addEventListener('click', addBook)
+  //Книгу в прочитанное
+  notReadedCol.addEventListener('click', moveBook);
+  //Книгу в не прочитанное
+  readedCol.addEventListener('click', moveBook);
+}
+
+addEvents();
+
+
 //const bookName = document.getElementById('book-name').value
 //console.log(addBookButton);
 const ul = document.querySelector('.not-readed');
@@ -18,39 +33,48 @@ function addBook(e) {
   //Добавляем содержимое
   li.innerHTML = `${bookName}
     <a href="#" class=secondary-content>
-    <i class="material-icons move right">Move</i></a>
+    <i class="material-icons move right">Переместить</i></a>
     <a href="#" class="secondary-content">
-    <i class="material-icons delete right">Delete</i></a>`
+    <i class="material-icons delete right">Удалить</i></a>`
   // Добавляем дочерний элемент к списку
   notReadedCol.appendChild(li);
 }
 
-addBookButton.addEventListener('click', addBook)
+
 //Удаление книги из списка
 function moveBook(e) {
     //Проверяем есть ли класс у элемента по которому кликнули
     if(e.target.classList.contains('move')){
-      const bookName = e.target.parentElement.parentElement.childNodes[0].textContent;
-      e.target.parentElement.parentElement.remove();
-      const li = document.createElement('li');
-      //Добавляем класс к элементу
-      li.className = 'collection-item';
-      //Добавляем содержимое для прочитанных книг
-      li.innerHTML = `${bookName}
-        <a href="#" class="secondary-content">
-        <i class="material-icons return right">Return</i></a>`
+      const bookName = e.target.parentElement.
+      parentElement.childNodes[0].textContent;
       // Добавляем дочерний элемент к списку
-      readedCol.appendChild(li);
+      if(e.target.parentElement.parentElement.parentElement.classList
+        .contains('not-readed')) {
+          const li = document.createElement('li');
+          li.className = 'collection-item';
+            li.innerHTML = `${bookName}
+              <a href="#" class="secondary-content">
+              <i class="material-icons move right">Переместить</i></a>`
+            readedCol.appendChild(li);
+           }
+      if (e.target.parentElement.parentElement.parentElement.classList
+        .contains('readed')) {
+          const li = document.createElement('li');
+            li.className = 'collection-item';
+            li.innerHTML = `${bookName}
+              <a href="#" class="secondary-content">
+              <i class="material-icons move right">Переместить</i></a>
+              <a href="#" class="secondary-content">
+              <i class="material-icons delete right">Удалить</i></a>`
+            notReadedCol.appendChild(li);
+      }
+      e.target.parentElement.parentElement.remove();
+
     }
   }
 
-notReadedCol.addEventListener('click', moveBook);
 //Перемещение книги в прочитанное
 function deleteBook() {
   // Получаем родительский элементу
-
-}
-
-function returnBook() {
 
 }
