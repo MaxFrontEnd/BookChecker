c9458bcda7ec90642625bd29ac706af573973593
Max <maxidromlordus@mail.ru>
2018-10-04 10:17:19 +0300

    refact move

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const notReadedCol = document.querySelector('.not-readed');
 const move = document.querySelector('.move');
 const addBookButton = document.getElementById('add-book');
 
-
 //евенты
 function addEvents(){
   // Добавить книгу в непрочитанное
@@ -52,26 +51,23 @@ function moveBook(e) {
     //Проверяем есть ли класс у элемента по которому кликнули
     if(e.target.classList.contains('move')){
       const bookName = e.target.parentElement.
-      parentElement.childNodes[0].textContent;
+      parentElement.childNodes[0].textContent.trim();
+      const ul = e.target.parentElement.parentElement.parentElement.classList;
       // Добавляем дочерний элемент к списку
-      if(e.target.parentElement.parentElement.parentElement.classList
-        .contains('not-readed')) {
-          const li = document.createElement('li');
-          li.className = 'collection-item';
-            li.innerHTML = `${bookName}
-              <a href="#" class="secondary-content">
-              <i class="material-icons move right">Переместить</i></a>`
+      const li = document.createElement('li');
+        li.className = 'collection-item';
+        li.innerHTML = `${bookName}
+          <a href="#" class="secondary-content">
+            <i class="material-icons move right">Переместить</i>
+          </a>
+          <a href="#" class="secondary-content">
+            <i class="material-icons delete right">Удалить</i>
+          </a>`
+      if(ul.contains('not-readed')) {
             readedCol.appendChild(li);
+            document.querySelector('.delete').remove();
            }
-      if (e.target.parentElement.parentElement.parentElement.classList
-        .contains('readed')) {
-          const li = document.createElement('li');
-            li.className = 'collection-item';
-            li.innerHTML = `${bookName}
-              <a href="#" class="secondary-content">
-              <i class="material-icons move right">Переместить</i></a>
-              <a href="#" class="secondary-content">
-              <i class="material-icons delete right">Удалить</i></a>`
+      if (ul.contains('readed')) {
             notReadedCol.appendChild(li);
       }
       e.target.parentElement.parentElement.remove();
